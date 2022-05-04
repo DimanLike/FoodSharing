e60a30bb0c5b0164f488a502c25406bbd6724010
DimanLike <dima.pozdn01@mail.ru>
2022-05-04 15:59:20 +0300

    UserProfileAlpha v1.0
    + Адаптивная страница профиля
    + Реализован переход для авторизированных пользователей

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodSharing.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+
+        [Route("Profile")]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
