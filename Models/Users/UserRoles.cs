9cca3612f6b7c394b16ffebd461ceffd950cc96d
DimanLike <dima.pozdn01@mail.ru>
2022-05-15 23:13:20 +0300

    Update DataBase

---
+++ Models/Users/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿namespace FoodSharing.Models.Users
+{
+    public class UserRoles
+    {
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+
+        public UserRoles (){}
+    }
+}
